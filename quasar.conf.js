08355b1107474c5dd777f1eeba787308cd673a7d
Pac <public@pa0x43.me>
2021-08-29 23:45:16 +0200

    Fix nginx and vue router

--- quasar.conf.js
+++ quasar.conf.js
@@ -55,8 +55,8 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
-      publicPath: 'pr0gramm_music_frontend',
+      vueRouterMode: 'history', // available values: 'hash', 'history'
+      //publicPath: 'pr0gramm_music_frontend',
 
       // transpile: false,
 
